f9a200be75c5bf64bdeab4bdea72af4278f4796a
LINUX <jmoc25@gmail.com>
2023-04-07 17:27:26 +0200

    chapter2

--- chapter2/code/subprocess/subprocess_ping_network.py
+++ chapter2/code/subprocess/subprocess_ping_network.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 from subprocess import Popen, PIPE
+import sys
 
+print("Operating system:",sys.platform)
+
+if sys.platform.startswith("linux"):
+    command_ping ='/bin/ping'
+elif sys.platform == "darwin":
+    command_ping ='/sbin/ping'
+elif os.name == "nt":
+	command_ping ='ping'
+	
 for ip in range(1,4):
 	ipAddress = '192.168.18.'+str(ip)
 	print("Scanning %s " %(ipAddress))
-	subprocess = Popen(['/bin/ping', '-c 1',ipAddress], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+	subprocess = Popen([command_ping, '-c 1',ipAddress], stdin=PIPE, stdout=PIPE, stderr=PIPE)
 	stdout, stderr= subprocess.communicate(input=None)
 	print(stdout)
 	if b"bytes from " in stdout:

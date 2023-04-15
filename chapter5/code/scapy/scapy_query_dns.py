03c93a44c27304d5c70a1594b79de30704baae69
LINUX <jmoc25@gmail.com>
2023-04-15 21:41:38 +0200

    chapter5

--- chapter5/code/scapy/scapy_query_dns.py
+++ chapter5/code/scapy/scapy_query_dns.py
@@ -5,6 +5,7 @@ from scapy.all import *
 def queryDNS(dnsServer,dominio):
     packet_dns= IP(dst=dnsServer)/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname=dominio))
     response_packet = sr1(packet_dns,verbose=1)
+    print(response_packet.show())
     return response_packet[DNS].summary()
     
 if __name__ == "__main__":

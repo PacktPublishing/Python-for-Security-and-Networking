6101ad3c43605ef0f54f6f3151708d53f9cf0a72
LINUX <jmoc25@gmail.com>
2023-05-16 18:41:42 +0200

    chapter3

--- chapter3/socket_service_names.py
+++ chapter3/socket_service_names.py
@@ -2,7 +2,7 @@ import socket
 
 def find_services_name():
     for port in [21,22,23,25,80]:
-        print("Port: %s => service name: %s" %(port, socket.	getservbyport(port, 'tcp')))
+        print("Port: %s => service name: %s" %(port, socket.getservbyport(port, 'tcp')))
     print("Port: %s => service name: %s" %(53, socket.getservbyport(53, 'udp')))
     
 if __name__ == '__main__':
